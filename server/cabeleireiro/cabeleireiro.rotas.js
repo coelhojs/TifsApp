559baab8b24fbb8fc4d45199fe4021a95f61dac8
Lucas <lucas.laborne@gmail.com>
2019-06-09 13:48:33 -0300

    Consertado os parametros da requisicao get (agora ele verifica a query, e nao o corpo da requisicao)

--- server/cabeleireiro/cabeleireiro.rotas.js
+++ server/cabeleireiro/cabeleireiro.rotas.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const Resposta = require('../resposta');
 
 router.get('/', (req, res, next) => {
-    Cabeleireiro.find(req.body)
+    Cabeleireiro.find(req.query)
         .exec()
         .then(result => { res.status(200).json(new Resposta(false, 200, '', result)); })
         .catch(err => { res.status(500).json(new Resposta(true, 500, err.message, err)); });
--- server/produto/produto.rotas.js
+++ server/produto/produto.rotas.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const Resposta = require('../resposta');
 
 router.get('/', (req, res, next) => {
-    Produto.find(req.body)
+    Produto.find(req.query)
         .exec()
         .then(result => { res.status(200).json(new Resposta(false, 200, '', result)); })
         .catch(err => { res.status(500).json(new Resposta(true, 500, err.message, err)); });
