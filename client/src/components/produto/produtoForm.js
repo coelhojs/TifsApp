17b45d2a9abcc49560f6fe00f5b9e062794f64de
coelhojs <ccoelho.me@gmail.com>
2019-06-13 02:26:03 -0300

    ajustes de ui

--- client/src/components/produto/produtoForm.js
+++ client/src/components/produto/produtoForm.js
@@ -13,41 +13,65 @@ class ProdutoForm extends Component {
         const { handleSubmit, pristine, reset, submitting } = this.props;
 
         return (
-            <form className="container formMobileProd" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <div className="text-center"><h1>Novo Produto</h1></div>
-                <hr />
-                <label>Categoria</label>
-                <Field name="categoria" component={renderField} className="renderField" />
-                <label>Marca</label>
-                <Field name="marca" component={renderField} className="renderField" type="text"
-                    placeholder="Ex.: Loreal"
-                />
-                <label>Linha</label>
-                <Field name="linha" label="Linha" component={renderField} className="renderField" type="text"
-                    placeholder="Ex.: Kids"
-                />
-                <label>Descrição</label>
-                <Field name="descricao" label="Descrição" component={renderField} className="renderField" type="text" />
-                <label>Conteúdo</label>
-                <Field name="conteudo" label="Conteúdo" component={renderField} className="form-control " type="number"
-                    placeholder="500"
-                />
-                {/* <Field name="medida" component="select">
+            <form className="container" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <div className="form-header text-center">
+                    <h1>Novo Produto</h1>
+                </div>
+                <div className="form-body offset-md-3 col-md-6">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <label>Categoria</label>
+                            <Field name="categoria" component={renderField} className="renderField" />
+                        </div>
+
+                        <div className="col-md-12">
+                            <label>Marca</label>
+                            <Field name="marca" component={renderField} className="renderField" type="text"
+                                placeholder="Ex.: Loreal"
+                            />
+                        </div>
+
+                        <div className="col-md-12">
+                            <label>Linha</label>
+                            <Field name="linha" label="Linha" component={renderField} className="renderField" type="text"
+                                placeholder="Ex.: Kids"
+                            />
+                        </div>
+
+                        <div className="col-md-12">
+                            <label>Descrição</label>
+                            <Field name="descricao" label="Descrição" component={renderField} className="renderField" type="text" />
+                        </div>
+
+                        <div className="col-md-12">
+                            <label>Conteúdo</label>
+                            <Field name="conteudo" label="Conteúdo" component={renderField} className="form-control " type="number"
+                                placeholder="500"
+                            />
+                        </div>
+
+                        {/* <Field name="medida" component="select">
                             <option value="ml">ml</option>
                             <option value="l">l</option>
                             <option value="mg">mg</option>
                             <option value="g">g</option>
                             <option value="kg">kg</option>
                         </Field> */}
-                {/* <Field name="medida" label="Medida" component={renderField} className="renderField"   /> */}
-                <div className="button-group d-flex justify-content-around">
-                    <button type="button" className="btn btn-danger" disabled={pristine || submitting}
-                        onClick={reset}>
-                        Cancelar
+                        {/* <Field name="medida" label="Medida" component={renderField} className="renderField"   /> */}
+                    </div>
+                </div>
+                <div className="form-footer row no-gutters">
+                    <div className="offset-md-3 col-md-6">
+                        <div className="button-group d-flex justify-content-around">
+                            <button type="button" className="btn btn-danger" disabled={pristine || submitting}
+                                onClick={reset}>
+                                Cancelar
                         </button>
-                    <button type="submit" className="btn btn-success" disabled={pristine || submitting}>
-                        Cadastrar
+                            <button type="submit" className="btn btn-success" disabled={pristine || submitting}>
+                                Cadastrar
                         </button>
+                        </div>
+                    </div>
                 </div>
                 <br />
                 {/* <p className="text-center">Dúvidas ao cadastrar o produto? <a href="">Clique aqui</a></p> */}
